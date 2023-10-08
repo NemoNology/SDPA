72c201eb7c43eb93bc282b90e774184135787c50
NemoNology <chicken.gang@mail.ru>
2023-10-08 20:21:06 +0900

    Added Dispose method

--- V/Lab's/1/C++/BinaryTreeNode.h
+++ V/Lab's/1/C++/BinaryTreeNode.h
@@ -1,4 +1,5 @@
 ﻿#pragma once
+#include <stack>
 
 using namespace std;
 
@@ -6,16 +7,66 @@ template <typename T>
 class BinaryTreeNode
 {
 public:
-	
 	T Value;
 
-	BinaryTreeNode* Left;
-	BinaryTreeNode* Right;
+	BinaryTreeNode *Left;
+	BinaryTreeNode *Right;
 
-	BinaryTreeNode(T value, BinaryTreeNode* left = null, BinaryTreeNode* right = null)
+	BinaryTreeNode(T value, BinaryTreeNode *left = nullptr, BinaryTreeNode *right = nullptr)
 	{
 		Value = value;
 		Left = left;
 		Right = right;
+	}
+
+	~BinaryTreeNode()
+	{
+		DisposeChildren();
+	}
+
+	void DisposeChildren()
+	{
+		stack<BinaryTreeNode *> s;
+		if (Left != nullptr)
+		{
+			s.push(Left);
+		}
+		if (Right != nullptr)
+		{
+			s.push(Right);
+		}
+
+		this->Left = nullptr;
+		this->Right = nullptr;
+
+		while (!s.empty())
+		{
+			BinaryTreeNode *node = s.top();
+			s.pop();
+
+			if (node->Left != nullptr)
+			{
+				s.push(node->Left);
+			}
+			if (node->Right != nullptr)
+			{
+				s.push(node->Right);
+			}
+
+			node->Left = nullptr;
+			node->Right = nullptr;
+
+			delete node;
+		}
+	}
+
+	string toString()
+	{
+		string l, r;
+
+		l = Left == nullptr ? "NULL" : to_string(Left->Value);
+		r = Right == nullptr ? "NULL" : to_string(Right->Value);
+
+		return "Value: " + to_string(Value) + "; Left: " + l + "; Right: " + r;
 	}
 };
