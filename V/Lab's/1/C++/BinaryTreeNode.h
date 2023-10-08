656151a7cbf6eb6694737fc11a4927c818c71f2f
NemoNology <chicken.gang@mail.ru>
2023-10-08 16:32:51 +0900

    Added binary tree node class without dispose

---
+++ V/Lab's/1/C++/BinaryTreeNode.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+using namespace std;
+
+template <typename T>
+class BinaryTreeNode
+{
+public:
+	
+	T Value;
+
+	BinaryTreeNode* Left;
+	BinaryTreeNode* Right;
+
+	BinaryTreeNode(T value, BinaryTreeNode* left = null, BinaryTreeNode* right = null)
+	{
+		Value = value;
+		Left = left;
+		Right = right;
+	}
+};
