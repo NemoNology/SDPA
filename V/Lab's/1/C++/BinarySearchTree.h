f050b2e9dd9b473e9fd42b983da91933c418c6eb
NemoNology <chicken.gang@mail.ru>
2023-10-13 03:53:52 +0900

    Started binary search tree class
    Completed: ctor & dector

---
+++ V/Lab's/1/C++/BinarySearchTree.h
@@ -0,0 +1,56 @@
+#include <iostream>
+#include "BinarySearchTreeNode.h"
+#include <stack>
+
+using namespace std;
+
+template <typename T>
+class BinarySearchTree
+{
+public:
+    BinarySearchTreeNode<T> *Root;
+
+    BinarySearchTree(BinarySearchTreeNode<T> *root)
+    {
+        Root = root;
+    }
+
+    ~BinarySearchTree<T>()
+    {
+        DisposeChildren();
+        delete Root;
+    }
+
+    void DisposeChildren()
+    {
+        auto nd{[](stack<BinarySearchTreeNode<T> *> &s, BinarySearchTreeNode<T> *&node)
+                {
+                    if (node->Left != nullptr)
+                    {
+                        s.push(node->Left);
+                        node->Left = nullptr;
+                    }
+                    if (node->Right != nullptr)
+                    {
+                        s.push(node->Right);
+                        node->Right = nullptr;
+                    }
+                }};
+
+        stack<BinarySearchTreeNode<T> *> s;
+        nd(s, Root);
+
+        while (!s.empty())
+        {
+            BinarySearchTreeNode<T> *node = s.top();
+            s.pop();
+            nd(s, node);
+            delete node;
+        }
+    }
+
+    string ToString()
+    {
+        return "Root: [" + Root->ToString() + "]";
+    }
+};
