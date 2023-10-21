b1d6472c5ae6547f77c5c39621a0b6f334706400
NemoNology <chicken.gang@mail.ru>
2023-10-21 12:21:42 +0900

    TODO: Make some tests

--- V/Lab's/Binary search tree/C++/Program.cpp
+++ V/Lab's/Binary search tree/C++/Program.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include "BinarySearchTree.h"
+#include <time.h>
 
 using namespace std;
 
@@ -7,25 +8,33 @@ using type = int;
 using TreeNode = BinarySearchTreeNode<type>;
 using Tree = BinarySearchTree<type>;
 
+int GetRandomInteger(int max = 20)
+{
+    return rand() % max;
+}
+
 int main()
 {
-    auto root = new TreeNode(4);
+    srand((unsigned int)time(nullptr));
+
+    auto root = new TreeNode(GetRandomInteger());
     auto tree = Tree(root);
 
-    tree.Add(1);
-    tree.Add(5);
-    tree.Add(6);
-    tree.Add(17);
-    tree.Add(11);
-    tree.Add(3);
-    pair<int, float> test;
+    for (int i = 0; i < 10; i++)
+        tree.Add(GetRandomInteger());
 
-    // cout << "Leaf count: " << tree.CountLeaf() << endl;
-    // cout << "Tree count: " << tree.Count() << endl;
-    // cout << "Tree depth: " << tree.GetDepth() << endl;
-    // cout << "Found node (11): " << tree.Find(11)->ToString() << endl;
-    // cout << "Contains value 11 ? - " << boolalpha << tree.Contains(11) << endl;
-    cout << "Found node (69): " << TreeNode::ToString(tree.Find(69)) << endl;
+    cout << "Start:\n"; 
+    tree.PrintVertical();
+    // tree.PrintHorizontalRecursive(tree.Root);
+
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     cout << "Deleting " << i << "(look down) :\n";
+    //     tree.Delete(i);
+    //     tree.PrintHorizontalRecursive(tree.Root);
+    // }
+
+    // cout << "Found node (69): " << TreeNode::ToString(tree.Find(69)) << endl;
 
     system("pause");
     return 0;
