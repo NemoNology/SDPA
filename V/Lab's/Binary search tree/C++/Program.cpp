ed497f9b712026aea4db80b2055dec8bf351ab7c
NemoNology <chicken.gang@mail.ru>
2023-10-27 02:53:14 +0900

    Started Delete Tree method tests

--- V/Lab's/Binary search tree/C++/Program.cpp
+++ V/Lab's/Binary search tree/C++/Program.cpp
@@ -8,30 +8,45 @@ using type = int;
 using TreeNode = BinarySearchTreeNode<type>;
 using Tree = BinarySearchTree<type>;
 
-int GetRandomInteger(int max = 20)
-{
-    return rand() % max;
-}
-
 int main()
 {
-    srand((unsigned int)time(nullptr));
     Tree tree = Tree();
 
-    for (int i = 0; i < 11; i++)
-        tree.Add(GetRandomInteger());
-
-    cout << "Start:\n";
+    tree.Add(50);
+    tree.Add(25);
+    tree.Add(75);
+    tree.Add(20);
+    tree.Add(30);
+    // cout << TreeNode::ToString(tree.Root) << " (Before)\n";
     tree.PrintVertical();
-    int size = 3;
+    // tree.Delete(0);
+    // cout << TreeNode::ToString(tree.Root) << " (After)\n";
+    // tree.PrintVertical();
 
-    for (int i = 0; i < size; i++)
-    {
-        int delitingNumber = GetRandomInteger();
-        cout << "Delete " << delitingNumber << "(\\|/) :\n";
-        tree.Delete(delitingNumber);
-        tree.PrintVertical();
-    }
+    // int size = 15;
+    // int *numbers = new int[size]{50, 25, 75, 20, 30, 70, 80, 15, 23, 28, 40, 65, 72, 76, 85};
+
+    // for (int i = 0; i < size; i++)
+    //     tree.Add(numbers[i]);
+
+    // cout << "Start:\n";
+    // tree.PrintVertical();
+
+    // delete numbers;
+    // size = 3;
+    // numbers = new int[size]{65, 40, 15};
+
+    // tree.Delete(65);
+    // cout << "Delete 65:\n\n";
+    // tree.PrintVertical();
+
+    // for (int i = 0; i < size; i++)
+    // {
+    //     int delitingNumber = numbers[i];
+    //     cout << "Delete " << delitingNumber << "(\\|/) :\n";
+    //     tree.Delete(delitingNumber);
+    //     tree.PrintVertical();
+    // }
 
     system("pause");
     return 0;
