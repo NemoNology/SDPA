8c468303faff20d0bf87f8dee78d0eaff8e232f3
NemoNology <chicken.gang@mail.ru>
2023-11-02 09:58:21 +0900

    Added: Heap class tests

---
+++ V/Lab's/Heap/C++/Program.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <ctime>
+#include "Heap.h"
+
+using namespace std;
+
+using type = int;
+
+int getRandomInt(int from, int to)
+{
+    return rand() % (to - from + 1) + from;
+}
+
+int main()
+{
+    srand(static_cast<unsigned int>(time(0)));
+    int size = 16;
+    Heap<type> heap = Heap<type>(size);
+
+    cout << "Inserting numbers: ";
+    for (int i = 0; i < size; i++)
+    {
+        int num = getRandomInt(0, 100);
+        cout << num << ", ";
+        heap.Insert(num);
+    }
+
+    cout << "\nHeap max: " << heap.GetMax() << "\nRemoving maxes:\n";
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << heap.GetMax() << " (" << heap.GetSize() << ") -> ";
+        heap.RemoveMax();
+    }
+
+    cout << "\n";
+
+    // system("pause");
+    return 0;
+}
