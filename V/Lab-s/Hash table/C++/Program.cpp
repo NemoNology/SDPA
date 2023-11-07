815b31049be414142e30e5335717978f788dd566
NemoNology <chicken.gang@mail.ru>
2023-11-07 09:41:04 +0900

    Added hash table (HT) & HT iterator tests

--- V/Lab-s/Hash table/C++/Program.cpp
+++ V/Lab-s/Hash table/C++/Program.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
+#include "HashTable.h"
+#include "HashFunctions.h"
+#include "HashTableIterator.h"
+#include <ctime>
 
 using namespace std;
 
+using type = int;
+using HT = HashTable<type>;
+using HF = HashFunctions;
+using HTI = HashTableIterator<type>;
+
 int main()
 {
-    
+    int size = 100;
+    HT ht = HT(size, HF::murmur3);
+    for (int i = 1; i <= size; i += 1)
+        ht.SetValue(to_string(i), i);
 
-    system("pause");
+    // HTI hti = HTI(&ht);
+
+    // for (hti.Reset(); !hti.IsIterationComplete(); hti.Next())
+    //     cout << hti.GetCurrent() << ", ";
+
+    // system("pause");
     return 0;
 }
